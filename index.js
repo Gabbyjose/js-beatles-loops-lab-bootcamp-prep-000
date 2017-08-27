993b13f097d9fbd2f076a09d0937e8d9398c8874
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-27 18:39:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
-  for (n=0; n < musicians.length-1; n++){
+  for (var n=0; n < musicians.length-1; n++){
     newArray.push(`${musicians[n]} plays ${instruments[n]}`);
   }
   return newArray;
